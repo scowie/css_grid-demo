44e705b4fcb66bb2577105c191191b7d845ecb18
Stephen Cowie <cowiestephenjames@gmail.com>
2017-12-24 08:49:23 -0500

    added vue material

--- app/index.js
+++ app/index.js
@@ -4,7 +4,10 @@ import vueResource from 'vue-resource'
 import vueRouter from 'vue-router'
 import appContainer from './components/app-container.vue'
 import demoPage from './components/demo-page.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
+vue.use(VueMaterial)
 vue.use(vueResource)
 vue.use(vueRouter)
 vue.config.devtools = false
